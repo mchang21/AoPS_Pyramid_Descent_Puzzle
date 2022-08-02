ada10bf7530dc6e5b56169f34eb1f5a23b07b359
Melvin Chang <melvchang@gmail.com>
2022-08-01 17:11:27 -0700

    update method parameters

--- Pyramid_Puzzle/pyramid.py
+++ Pyramid_Puzzle/pyramid.py
@@ -1,6 +1,7 @@
 class Pyramid:
-    def __init__(self):
-        pass
+    def __init__(self, rows):
+        self.rows = rows
 
-    def findPath(self):
+    def findPath(self, target, parent_idx = 0, current_row_idx=1,
+     current_product=None, current_path = ""):
         pass
